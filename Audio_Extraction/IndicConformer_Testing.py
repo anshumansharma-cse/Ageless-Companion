6c731160a783876015c62e07c85f79c8e1960c77
Anshuman Sharma <anshumansharma1105@gmail.com>
2026-08-29 23:06:19 +0530

    Create IndicConformer_Testing.py

---
+++ Audio_Extraction/IndicConformer_Testing.py
@@ -0,0 +1,7 @@
+"""
+Ageless STT Module-2
+---------------------------------------------------------
+AGELESS Companion : STT Testing Pipeline (Terminal Prints the transcription)
+---------------------------------------------------------
+IndicConformer Testing
+"""
